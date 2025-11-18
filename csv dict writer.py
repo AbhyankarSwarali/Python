fe4fc4735495c7f409b6f8a162f1c4a11179c47f
Abhyankar Swarali <swaraliabhyankar1303@gmail.com>
2025-11-18 09:46:35 +0530

    Add files via upload

---
+++ csv dict writer.py
@@ -0,0 +1,19 @@
+import csv
+
+covid = [
+    {'Country':'India', 'Doses':'186Cr', 'People':'84.1Cr', 'Percentage':61},
+    {'Country':'China', 'Doses':'330Cr', 'People':'124Cr', 'Percentage':88.1},
+    {'Country':'United States', 'Doses':'56.8Cr', 'People':'21.9Cr', 'Percentage':66.4},
+    {'Country':'Brazil', 'Doses':'42.4Cr', 'People':'16.2Cr', 'Percentage':76.4},
+    {'Country':'Indonesia', 'Doses':'39Cr', 'People':'16.2Cr', 'Percentage':59.3}
+]
+
+f = open('Covid_Dict.csv', 'w', newline='')
+fields = ['Country', 'Doses', 'People', 'Percentage']
+writer = csv.DictWriter(f, fieldnames=fields)
+writer.writeheader()
+
+for c in covid:
+    writer.writerow(c)
+
+f.close()
